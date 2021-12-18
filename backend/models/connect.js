0e7de3fa63a0add597d539608e7bdb612f7bddeb
Charles Cunningham <mureytasroc@gmail.com>
2021-12-18 00:10:27 -0500

    Update connect.js

--- backend/models/connect.js
+++ backend/models/connect.js
@@ -1,8 +1,9 @@
-import dynamo from 'dynamodb';
-import './Chat.js';
-import './News.js';
-import './Post.js';
-import './User.js';
+import { Chat, ChatHistory } from './Chat.js';
+import { Friendship } from './Friendship.js';
+import { Category, Article, ArticleLike, ArticleKeyword,
+  ArticleRanking, RecommendedArticle } from './News.js';
+import { Post, Comment } from './Post.js';
+import { Affiliation, User, UserAutocomplete } from './User.js';
 import redis from 'ioredis';
 
 export const redisClient = process.env.REDIS_URL ?
@@ -13,8 +14,30 @@ redisClient.on('error', (err) => {
   throw err;
 });
 
-dynamo.createTables(function(err) {
-  if (err && err.code !== 'ResourceInUseException') {
-    throw err;
+const tableAndOptions = [
+  [Chat, {}],
+  [ChatHistory, {}],
+  [Friendship, { 'BillingMode': 'PAY_PER_REQUEST' }],
+  [Category, {}],
+  [Article, { 'BillingMode': 'PAY_PER_REQUEST' }],
+  [ArticleLike, { 'BillingMode': 'PAY_PER_REQUEST' }],
+  [ArticleKeyword, { 'BillingMode': 'PAY_PER_REQUEST' }],
+  [ArticleRanking, { 'BillingMode': 'PAY_PER_REQUEST' }],
+  [RecommendedArticle, { 'BillingMode': 'PAY_PER_REQUEST' }],
+  [Post, {}],
+  [Comment, {}],
+  [Affiliation, { 'BillingMode': 'PAY_PER_REQUEST' }],
+  [User, {}],
+  [UserAutocomplete, {}],
+];
+
+for (const [table, options] of tableAndOptions) {
+  try {
+    await new Promise((resolve, reject) =>
+      table.createTable(options, (err) => err ? reject(err) : resolve()));
+  } catch (err) {
+    if (err.code !== 'ResourceInUseException') {
+      throw err;
+    }
   }
-});
+}
